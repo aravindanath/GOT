aaa915a0042f2bb731d1cc7b68a58d907ce37a32
aravindanath <aravindanath86@gmail.com>
2020-01-12 10:55:27 +0530

    base poge

---
+++ src/test/java/ReUsables/BaseClass.java
@@ -0,0 +1,38 @@
+package ReUsables;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.ini4j.Ini;
+
+public class BaseClass {
+
+	/**
+	 * @author aravindanathdm
+	 * @param key
+	 * @return
+	 * @throws IOException
+	 * 
+	 */
+	public static String getValue(String key) throws IOException {
+		String path = System.getProperty("user.dir") + "//TestData//testdata.properties";
+		FileInputStream fis = new FileInputStream(path);
+		Properties prop = new Properties();
+		prop.load(fis);
+		String urlDev = prop.getProperty(key);
+		return urlDev;
+
+	}
+
+	public static String getValue(String header, String key) throws IOException {
+		String path = System.getProperty("user.dir") + "//TestData//testData.ini";
+		FileInputStream fis = new FileInputStream(path);
+		Ini ini = new Ini();
+		ini.load(fis);
+		String val = ini.get(header, key);
+		return val;
+
+	}
+
+}
