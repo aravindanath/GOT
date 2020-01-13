7f2865aec74a8f68c07ab21946bad52bcfb5a878
praveena44 <praveenadurairaj44@gmail.com>
2020-01-13 19:32:16 +0530

    File Handling Assignment

---
+++ src/test/java/assignment/Praveena_RW_Properties.java
@@ -0,0 +1,30 @@
+package assignment;
+
+import java.io.IOException;
+import org.testng.annotations.*;
+
+public class Praveena_RW_Properties extends Praveena_Base_Class {
+
+	// Creating a properties file, writing in to it and reading the written key
+	// value pair.
+
+	// To create new properties file through code
+	String path = System.getProperty("user.dir") + "//TestDataj//Praveena_Test_Data_Prop.properties";
+
+	// To write in to properties file
+	@Test(priority = 1)
+	public void writePropFile() throws IOException {
+		Praveena_Base_Class.writeProperties("username", "praveena", path);
+		Praveena_Base_Class.writeProperties("password", "selenium123", path);
+		Praveena_Base_Class.writeProperties("url", "localhost", path);
+	}
+
+	// To read from properties file
+	@Test(priority = 2)
+	public void readPropFile() throws IOException {
+		String value = Praveena_Base_Class.readProperties("password", path);
+		System.out.println("Value for key is " + value);
+		String value1 = Praveena_Base_Class.readProperties("username", path);
+		System.out.println("Value for key is " + value1);		
+	}
+}
