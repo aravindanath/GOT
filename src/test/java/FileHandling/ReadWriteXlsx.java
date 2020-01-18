7842257b82ccd7e7684bf74fc984c9d61fd6ed12
aravindanath <aravindanath86@gmail.com>
2020-01-18 12:58:44 +0530

    xlsx

---
+++ src/test/java/FileHandling/ReadWriteXlsx.java
@@ -0,0 +1,17 @@
+package FileHandling;
+
+public class ReadWriteXlsx {
+	
+	public static void main(String[] args) {
+		
+		String path = System.getProperty("user.dir")+"//TestData//TestData.xlsx";
+		
+		Xls_Reader xls = new Xls_Reader(path);
+		String sheetName ="Control";
+		String val = xls.getCellData(sheetName, 4, 1);
+		String cal =xls.getCellData(sheetName, "Runmode", 3);
+		System.out.println(cal);
+		
+	}
+
+}
