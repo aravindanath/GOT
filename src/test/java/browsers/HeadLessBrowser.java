e35fea0691dfced9d58593a2b23325ba668e174f
aravindanath <aravindanath86@gmail.com>
2020-02-01 12:08:19 +0530

    updaTED headless

--- src/test/java/browsers/HeadLessBrowser.java
+++ src/test/java/browsers/HeadLessBrowser.java
@@ -2,15 +2,17 @@ package browsers;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class HeadLessBrowser extends LaunchBrowserUsingWebDriverManager{
-	
-	
+public class HeadLessBrowser {
+
+	WebDriver driver; 
 	@BeforeTest
 	public void setup() {
 		ChromeOptions ops = new ChromeOptions();
